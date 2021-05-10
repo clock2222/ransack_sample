06b28cb697ab589144f9d4b6e9fd15ad1e6c76b8
clocknote2222 <clocknote2022@gmail.com>
2021-05-10 15:23:41 +0900

    検索機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  # 1ページの表示数
+  PER_PAGE = 20
+
+  def index
+    # ***** 以下を修正 *****
+    @q = User.ransack(params[:q])
+    @users = @q.result.page(params[:page]).per(PER_PAGE)
+    # ***** 以上を修正 *****
+  end
+end
